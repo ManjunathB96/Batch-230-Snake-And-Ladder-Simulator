1f87c463e961c444630c4e16a2800b4a97ee7764
Manju31051996 <bbelagavi6@gmail.com>
2022-12-28 23:00:55 +0530

    [Manjunath S B] Day-4 ->Snake And Ladder : Welcome message is added

---
+++ src/com/bridgelabz_snake_and_ladder_simulator/SnakeAndLadder.java
@@ -0,0 +1,7 @@
+package com.bridgelabz_snake_and_ladder_simulator;
+
+public class SnakeAndLadder {
+    public static void main(String[] args) {
+        System.out.println("Welcome To Snake And Ladder Simulator....!");
+    }
+}
